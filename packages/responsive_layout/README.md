0357e65e83c090d31ce2d3a40bf4dafe3d72412d
Sandip <sandipramanik07@gmail.com>
2024-08-26 01:45:49 +0530

    chore(packages): create package: responsive_layout

---
+++ packages/responsive_layout/README.md
@@ -0,0 +1,3 @@
+# Responsive Layout
+
+A wrapper around LayoutBuilder which exposes builders for various responsive breakpoints
---
+++ packages/responsive_layout/lib/responsive_layout.dart
@@ -0,0 +1,7 @@
+library responsive_layout;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
